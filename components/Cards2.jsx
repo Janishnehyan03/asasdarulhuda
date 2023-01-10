d715db0b1921618d31eb902a75048a687eb5585d
Janishnehyan03 <nehyanjanish@gmail.com>
2023-01-10 17:39:55 +0530

    initial commit

---
+++ components/Cards2.jsx
@@ -0,0 +1,138 @@
+import React from "react";
+
+function Cards2() {
+  return (
+    <>
+      <div className="sm:text-center">
+        <h2 className="text-lg font-semibold leading-8 text-indigo-600">
+          Transactions
+        </h2>
+        <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          A better way to send money
+        </p>
+        <p className="mx-auto mt-6 max-w-2xl text-lg leading-8 text-gray-600">
+          Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam
+          voluptatum cupiditate veritatis in accusamus quisquam.
+        </p>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-2 m-3">
+        <a
+          href="#"
+          className="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 "
+        >
+          <img
+            className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+            src="/images/image-4.jpg"
+            alt
+          />
+          <div className="flex flex-col justify-between p-4 leading-normal">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+              Noteworthy technology acquisitions 2021
+            </h5>
+            <p className="mb-3 font-normal text-gray-700 ">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+          </div>
+        </a>
+        <a
+          href="#"
+          className="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 "
+        >
+          <img
+            className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+            src="/images/image-4.jpg"
+            alt
+          />
+          <div className="flex flex-col justify-between p-4 leading-normal">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+              Noteworthy technology acquisitions 2021
+            </h5>
+            <p className="mb-3 font-normal text-gray-700 ">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+          </div>
+        </a>
+        <a
+          href="#"
+          className="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 "
+        >
+          <img
+            className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+            src="/images/image-4.jpg"
+            alt
+          />
+          <div className="flex flex-col justify-between p-4 leading-normal">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+              Noteworthy technology acquisitions 2021
+            </h5>
+            <p className="mb-3 font-normal text-gray-700 ">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+          </div>
+        </a>
+        <a
+          href="#"
+          className="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 "
+        >
+          <img
+            className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+            src="/images/image-4.jpg"
+            alt
+          />
+          <div className="flex flex-col justify-between p-4 leading-normal">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+              Noteworthy technology acquisitions 2021
+            </h5>
+            <p className="mb-3 font-normal text-gray-700 ">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+          </div>
+        </a>
+        <a
+          href="#"
+          className="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 "
+        >
+          <img
+            className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+            src="/images/image-4.jpg"
+            alt
+          />
+          <div className="flex flex-col justify-between p-4 leading-normal">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+              Noteworthy technology acquisitions 2021
+            </h5>
+            <p className="mb-3 font-normal text-gray-700 ">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+          </div>
+        </a>
+        <a
+          href="#"
+          className="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 "
+        >
+          <img
+            className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+            src="/images/image-4.jpg"
+            alt
+          />
+          <div className="flex flex-col justify-between p-4 leading-normal">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+              Noteworthy technology acquisitions 2021
+            </h5>
+            <p className="mb-3 font-normal text-gray-700 ">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+          </div>
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default Cards2;
